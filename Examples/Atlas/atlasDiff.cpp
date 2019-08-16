3e9f6df9c46cb1dcccf54c80315192b31c53cc8b
Tobias Wicky <twicki@ethz.ch>
2019-08-16 17:08:24 +0200

    (most likely wrong) atlas examples

---
+++ Examples/Atlas/atlasDiff.cpp
@@ -0,0 +1,61 @@
+#include "atlas/field/Field.h"
+#include "atlas/functionspace/NodeColumns.h"
+#include "atlas/grid/Grid.h"
+#include "atlas/grid/StructuredGrid.h"
+#include "atlas/grid/UnstructuredGrid.h"
+#include "atlas/mesh/Connectivity.h"
+#include "atlas/mesh/HybridElements.h"
+#include "atlas/mesh/Mesh.h"
+#include "atlas/mesh/Nodes.h"
+#include "atlas/meshgenerator.h"
+#include "atlas/meshgenerator/MeshGenerator.h"
+#include "atlas/meshgenerator/detail/DelaunayMeshGenerator.h"
+#include "atlas/output/Gmsh.h"
+#include "atlas/util/Config.h"
+#include "eckit/types/FloatCompare.h"
+#include <array/ArrayShape.h>
+#include <array/DataType.h>
+#include <array_fwd.h>
+#include <iostream>
+#include <sstream>
+
+int main() {
+  ////////////////////// Regular Grid setup
+  // Grid setup: 10 x 10 lat lon points
+  atlas::StructuredGrid structuredGrid = atlas::Grid("L50x49");
+  std::cout << "grid.npts() = " << structuredGrid.size() << std::endl;
+
+  // Mesh setup to this grid (quads)
+  atlas::StructuredMeshGenerator generator;
+  auto mesh = generator.generate(structuredGrid);
+  atlas::output::Gmsh gmesh("mymesh.msh");
+  gmesh.write(mesh);
+
+  // Field
+  atlas::functionspace::NodeColumns fs_nodes(mesh, atlas::option::levels(1));
+  auto field_temp2 = fs_nodes.createField<double>(atlas::option::name("temperature"));
+  auto temp2 = atlas::array::make_view<double, 2>(field_temp2);
+
+  // Initialize everything with a nice gaussian
+  auto lonlat = atlas::array::make_view<double, 2>(mesh.nodes().lonlat());
+  const double rpi = 2.0 * asin(1.0);
+  const double deg2rad = rpi / 180.;
+  double width = 0.1;
+  for(int jNode = 0, size = mesh.nodes().size(); jNode < size; ++jNode) {
+    double iCoordinate = lonlat(jNode, 0) * deg2rad;
+    double jCoordinate = lonlat(jNode, 1) * deg2rad + 1;
+    temp2(jNode, 0) =
+        exp(-width * (pow(iCoordinate - 6.0 / 2.0, 2) + pow(jCoordinate - 2.0 / 2.0, 2)));
+  }
+
+  // time stepping
+  for(int step = 0; step < timeSteps - 1; step++) {
+  }
+
+  auto& ect = mesh.nodes().edge_connectivity();
+  std::cout << mesh.nodes().cell_connectivity() << std::endl;
+
+  std::cout << "edgeconsize: " << ect.size() << std::endl;
+  gmesh.write(field_temp2);
+  return 0;
+}
--- Examples/atlasexample.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "atlas/field.h"
-
-int main(){
-    std::cout << "hello" << std::endl;
-}
