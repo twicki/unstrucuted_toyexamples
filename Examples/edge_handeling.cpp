96f17a71524f6b776ff2ac6116fbfa486bc95bec
Tobias Wicky <twicki12@gmail.com>
2019-10-15 16:38:54 +0200

    edge will copy halo

---
+++ Examples/edge_handeling.cpp
@@ -0,0 +1,30 @@
+#include "grid.h"
+#include <cmath>
+#include <iostream>
+
+int main(int argc, char const* argv[]) {
+  std::cout << "hello" << std::endl;
+  int horizontal = 20;
+  int nHalo = 2;
+  Grid g(horizontal, nHalo);
+  double width = 0.01;
+  for(int i = 0; i < horizontal; ++i) {
+    for(int j = 0; j < horizontal; ++j) {
+      g(i, j) = exp(-width *
+                    (pow(i - double(horizontal) / 2.0, 2) + pow(j - double(horizontal) / 2.0, 2)));
+    }
+  }
+
+  double dt5 = 0.5;
+  for(int i = 0; i < 1; ++i) {
+    for(int j = 0; j < horizontal; ++j) {
+      g(i, j) = g(i, j - 1);
+    }
+  }
+
+  GridPrinter p("halo.vtk");
+  p.printWithHalo(g);
+  GridPrinter p2("nohalo.vtk");
+  p2.printInterior(g);
+  return 0;
+}
