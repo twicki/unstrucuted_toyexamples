cea3ece69fae4f29ab5afcf757a8fe54f5baa75b
Tobias Wicky <twicki12@gmail.com>
2019-10-15 16:14:29 +0200

    better naming

--- Examples/simple.cpp
+++ Examples/simple.cpp
@@ -4,12 +4,14 @@
 
 int main(int argc, char const* argv[]) {
   std::cout << "hello" << std::endl;
-  int x = 20;
-  Grid g(x, 2);
+  int horizontal = 20;
+  int nHalo = 2;
+  Grid g(horizontal, nHalo);
   double width = 0.01;
-  for(int i = 0; i < x; ++i) {
-    for(int j = 0; j < x; ++j) {
-      g(i, j) = exp(-width * (pow(i - double(x) / 2.0, 2) + pow(j - double(x) / 2.0, 2)));
+  for(int i = 0; i < horizontal; ++i) {
+    for(int j = 0; j < horizontal; ++j) {
+      g(i, j) = exp(-width *
+                    (pow(i - double(horizontal) / 2.0, 2) + pow(j - double(horizontal) / 2.0, 2)));
     }
   }
   GridPrinter p("halo.vtk");
